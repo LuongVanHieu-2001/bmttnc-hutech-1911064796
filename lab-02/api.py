901249e555c66a6cac3954b6f82a93278964dc57
luongvanhieu01 <luongvanhieu01@gmail.com>
2024-04-10 08:10:25 +0700

    [add lab02 web caesar

--- lab-02/api.py
+++ lab-02/api.py
@@ -3,8 +3,28 @@ from flask import Flask, request, jsonify
 from cipher.caesar.caesar_cipher import CaesarCipher
 from cipher.vigenere.vigenere_cipher import VigenereCipher
 from cipher.playfair.playfair_cipher import PlayFairCipher
+from cipher.transposition.transposition_cipher import TranspositionCipher
 
 app = Flask (__name__)
+
+transposition_cipher = TranspositionCipher ()
+@app.route("/api/transposition/encrypt", methods=["POST"])
+def transposition_encrypt():
+    data=request.json
+    plain_text=data['plain_text']
+    key= int(data['key'])
+    encrypted_text = transposition_cipher.encrypt (plain_text, key)
+    return jsonify({'encrypted message': encrypted_text})
+@app.route("/api/transposition/decrypt", methods=["POST"])
+def transposition_decrypt():
+    data = request.json
+    cipher_text = data['cipher_text']
+    key = int (data['key'])
+    decrypted_text = transposition_cipher.decrypt (cipher_text, key)
+    return jsonify ({'decrypted_message': decrypted_text})
+
+
+
 
 playfair_cipher = PlayFairCipher ()
 @app.route('/api/playfair/creatematrix', methods=['POST'])
@@ -30,24 +50,22 @@ def playfair_decrypt ():
     decrypted_text = playfair_cipher.playfair_decrypt(cipher_text,playfair_matrix)
     return jsonify ({'decrypted_text': decrypted_text})
 
-#RAILFENCE CIPHER ALGORITHM
-railfence_cipher = RailFenceCipher
-
+railfence_cipher = RailFenceCipher ()
 @app.route("/api/railfence/encrypt", methods=["POST"])
 def encrypt():
-    data = request.json
-    plain_text = data ['plain_text']
-    key = int(data['key'])
-    encrypted_text = railfence_cipher.rail_fence_encrypt (plain_text, key)
-    return jsonify({'encrypted_text': encrypted_text})
-
-@app.route("/api/railfence/decrypt", methods=["POST"]) 
-def decrypt():  
-    data = request.json
-    cipher_text = data ['cipher_text']
+    data=request.json
+    plain_text=data['plain_text']
     key= int(data['key'])
-    decrypted_text = railfence_cipher.rail_fence_dedcrypt (cipher_text, key)
-    return jsonify({'decrypt_text': decrypted_text})
+    encrypted_text = railfence_cipher.rail_fence_encrypt (plain_text, key)
+    return jsonify({'encrypted message': encrypted_text})
+@app.route("/api/railfence/decrypt", methods=["POST"])
+def decrypt():
+    data = request.json
+    cipher_text = data['cipher_text']
+    key = int (data['key'])
+    decrypted_text = railfence_cipher.rail_fence_decrypt (cipher_text, key)
+    return jsonify ({'decrypted_message': decrypted_text})
+
     
     
 caesar_cipher = CaesarCipher ()
